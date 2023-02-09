7897c7830c021ee3f6f5505932506ed67187feae
Edson Bastos <abastos@uolin.com>
2023-02-09 13:37:41 -0300

    feat: adicionado metodo para incrementar e diminuir quantidade

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,19 +1,44 @@
 import { Avatar, ListItem, ListItemAvatar, ListItemText, Grid, Typography } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import DadosContext from '../../contexts/DadosContext';
 
-function CardProduct(props) {
+function CardProduct( props ) {
 
     const { productsOrders, setProductsOrders } = useContext(DadosContext);
+    const [products, setProducts] = useState({})
 
-    function addProduct(item) {
-        let list = productsOrders;
-        list.push(item);
+    function addToCard(array, obj, index) {
+        const list = array
+        list.push({...obj, qtd: 1})
         setProductsOrders([...list]);
     }
 
-    function removeProduct(item) {
+    function increaseQuantity(array, index) {
+        ++array[index].qtd
+        console.log('increaseQuantity: ',array[index])
+    }
 
+    function decreaseQuantity(array, index) {
+        if(array[index].qtd > 0 ) {
+            --array[index].qtd
+            return
+        }
+    }
+
+    function addProduct(item) {
+        let list = productsOrders
+        const cartItem = list.find(itemList => itemList.id === item.id)
+        const index = list.findIndex(itemList => itemList.id === item.id)
+        cartItem ? increaseQuantity(list, index) : addToCard(list, item, index)
+        console.log('addProduct: ',list)
+        console.log('products: ', products)
+    }
+
+    function removeProduct(item) {
+        let list = productsOrders
+        const index = list.findIndex(itemList => itemList.id === item.id)
+        decreaseQuantity(list, index)
+        console.log(list)
     }
 
     return (
@@ -32,6 +57,7 @@ function CardProduct(props) {
                     <Typography variant="p">
                         R$ {props.price}
                         <button onClick={() => removeProduct(props)}>-</button>
+                        <span>{props.qtd}</span>
                         <button onClick={() => addProduct(props)}>+</button>
                     </Typography>
                 </Grid>
