0f0d05a4d5423174ea0dd427f2fd142f63fc8065
Edson Bastos <abastos@uolin.com>
2023-02-13 16:02:23 -0300

    style: modificado layout da página

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -3,19 +3,31 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import ListCard from "../../components/ListCard";
 import DadosContext from '../../contexts/DadosContext'
+import {ButtonDefault} from '../../components/ButtonDefault'
+import { Box } from "@mui/system";
+import {Grid} from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 function Orders() {
     const  { productsOrders, setProductsOrders } = useContext(DadosContext)
     return (
-        <>
-            <h1>Pedidos</h1>
-            <ListCard list={productsOrders} />
-            <Button variant='contained' fullWidth >
-                <Link to="/address">Finalizar</Link>
-            </Button>
-            <Button variant='contained' fullWidth >
-                <Link to="/">Voltar</Link>
-            </Button>
+        <>  
+        <Grid container>
+            <Grid item xs={12}>
+                <Button variant="contained" sx={{m:2}} startIcon={<ArrowBackIosNewIcon/>} >
+                    <Link to="/">Voltar</Link>
+                </Button>
+                <h1>Pedidos</h1>
+            </Grid>
+            <Grid item xs={12}>
+                <ListCard list={productsOrders} />
+            </Grid>
+            <Grid item xs={12} style={{position: 'absolute', bottom:0, left:0, width:'100%'}}>
+                <ButtonDefault variant='contained'>
+                    <Link to="/address">Finalizar</Link>
+                </ButtonDefault>
+            </Grid>
+        </Grid>
         </>
     );
 }
