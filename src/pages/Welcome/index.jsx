83ebc07d059e4198895025824ea320919f071c34
Edson Bastos <abastos@uolin.com>
2023-02-16 20:55:46 -0300

    feat: criado componente de slider

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,20 +1,37 @@
-import { Avatar, Button, CssBaseline, Grid, TextField } from '@mui/material';
+import { Avatar, Button, CssBaseline, Grid, TextField, Typography } from '@mui/material';
 import ListCard from '../../components/ListCard';
 import { Link } from 'react-router-dom';
 import pizza from '../../assets/images/pizza.png';
-import pastel from '../../assets/images/pastel.png';
-import hamburguer from '../../assets/images/hamburguer.png';
-import banner from '../../assets/images/banner.jpg';
 import { Box } from '@mui/system';
 import { useState } from 'react';
 import api from '../../config/api';
 import { useEffect } from 'react';
 
-function Welcome() {
+import {Slider, Slide} from '../../components/SliderShow'
+import banner from '../../assets/images/banner.jpg'
+import banner2 from '../../assets/images/banner-2.jpg'
+import banner3 from '../../assets/images/banner-3.jpg'
+import banner1 from '../../assets/images/banner-1.jpg'
 
+const productImages = [banner1, banner2, banner3, banner]
+
+
+function Welcome() {
     const[categories, setCategories] = useState([]);
     const[banners, setBanners] = useState([]);
     const[products, setProducts] = useState([]);
+
+    const settings = {
+        spaceBetween: 5,
+        slidesPerView: 1,
+        navigation: false,
+        draggable: true,
+        loop: false,
+        pagination: {
+            clickable: true
+        }
+    }
+
     const styles = {
         box: {
             width: '100%', 
@@ -82,11 +99,19 @@ function Welcome() {
                     </div>
                 </Box>
                 <Grid container>
-                    {banners.map((item, index) => (
-                        <Grid key={index}>
-                            <img src={banner} alt={item.description} id="banner"/>
-                        </Grid>
-                    ))}
+                    <Typography sx={{fontSize:'1.25rem', lineHeight:'1.625rem', padding:'20px 0', color:'#1a1a1a' }} variant="h4" gutterBottom>
+                        As melhores ofertas
+                    </Typography>
+                    <Slider settings={settings}>
+                        {
+                            productImages.map((item, index) => (
+                                <Slide key={index}>
+                                    
+                                    <img src={item} alt="" />
+                                </Slide>
+                            ))
+                        }
+                    </Slider>
                 </Grid>
 
                 <Grid container>
